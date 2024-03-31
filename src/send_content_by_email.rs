d04b88e9f73cdef68b52a077553a4675a2d77d13
Yassine Saddiki <yassine.saddiki@etu.unilim.fr>
2024-03-31 23:46:23 +0200

    modification find_by_id()

--- src/send_content_by_email.rs
+++ src/send_content_by_email.rs
@@ -11,10 +11,15 @@ pub enum SendEmailError {
     InvalidEmailError,
     InvalidUserInfoError(String),
 }
-
-impl From<std::io::Error> for SendEmailError {
+//Cette partie définit une implémentation de la trait From, qui permet de convertir un type 
+//en un autre. Ici, nous convertissons std::io::Error en SendEmailError.
+impl From<std::io::Error> for SendEmailError { 
     fn from(error: std::io::Error) -> Self {
+//C'est la méthode associée à l'implémentation de From. Elle prend une erreur de type std::io::Error 
+//en entrée et renvoie notre propre type d'erreur SendEmailError.
         SendEmailError::IOError(error)
+//Cette partie crée une nouvelle instance de SendEmailError avec la variante IOError, qui contient 
+//l'erreur std::io::Error d'origine. 
     }
 }
 
@@ -27,10 +32,7 @@ type SendEmailResult<T> = Result<T, SendEmailError>;
 
 pub fn send_content_by_email(file_path: &str, user_id: &str) -> SendEmailResult<String> {
     let data = read_file(file_path)?;
-    let user_info = match user::find_by_id(user_id) {
-        Some(user) => user,
-        None => return Err(SendEmailError::UserNotFoundError),
-    };
+    let user_info = user::find_by_id(user_id)?;
     let validated_info = validate_user_info(&user_info)?;
     send_email(&validated_info.email_address, &validated_info.first_name, &validated_info.last_name, &data)?;
     Ok("Terminé".to_string())
--- src/user.rs
+++ src/user.rs
@@ -1,4 +1,4 @@
-
+use crate::send_content_by_email::SendEmailError;
 pub struct User {
     pub id: String,
     pub first_name: String,
@@ -7,21 +7,20 @@ pub struct User {
 }
 
 
-pub fn find_by_id(user_id: &str) -> Option<User> {
-    
+pub fn find_by_id(user_id: &str) -> Result<User, SendEmailError> {
     match user_id {
-        "1" => Some(User {
+        "1" => Ok(User {
             id: "1".to_string(),
             first_name: "John".to_string(),
             last_name: "Doe".to_string(),
             email_address: "john@example.com".to_string(),
         }),
-        "2" => Some(User {
+        "2" => Ok(User {
             id: "2".to_string(),
             first_name: "Jane".to_string(),
             last_name: "Doe".to_string(),
             email_address: "jane@example.com".to_string(),
         }),
-        _ => None,
+        _ => Err(SendEmailError::UserNotFoundError),
     }
 }
